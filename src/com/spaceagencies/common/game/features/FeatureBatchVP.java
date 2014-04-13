54186ab977440e722fa15453a22bed806e85a5ce
Frédéric bertolus <fred.bertolus@gmail.com>
2014-04-13 16:23:26 +0200

    [DEV] Add features

---
+++ src/com/spaceagencies/common/game/features/FeatureBatchVP.java
@@ -0,0 +1,35 @@
+package com.spaceagencies.common.game.features;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.spaceagencies.common.game.Card;
+import com.spaceagencies.common.game.CardFeature;
+import com.spaceagencies.common.game.Turn;
+
+@XmlRootElement(name="batchVP")
+public class FeatureBatchVP extends CardFeature {
+    
+    @XmlAttribute
+    private int count;
+
+    public FeatureBatchVP() {
+        super();
+    }
+    
+    public FeatureBatchVP(int count) {
+        super();
+        this.count = count;
+    }
+
+
+    @Override
+    public void resolve(Turn t) {
+    }
+    
+    @Override
+    public String getDescription() {
+        return "";
+    }
+
+}
